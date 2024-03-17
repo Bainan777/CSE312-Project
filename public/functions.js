9ba642b89b9e0f9e141ea2d2355c685eb684d2cc
ibtidaah217 <89950873+ibtidaah217@users.noreply.github.com>
2024-03-17 16:39:57 -0400

    HTML and JS tweaked to accomodate authentication.

--- public/functions.js
+++ public/functions.js
@@ -1,3 +1,4 @@
 function welcome(){
-    alert("This is a placeholder Javascript function! Expect more in the coming future.")
+    alert("This is a placeholder Javascript function! Expect more in the coming future.");
+    setTimeout(3000);
 }
