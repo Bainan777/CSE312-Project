3bbd01bd9622b8cd270f874f1bb7ac532c369f0d
Jessica Mui <jessicamui90@gmail.com>
2024-03-25 08:47:54 -0400

    commented back in 'welcome' js function

--- public/functions.js
+++ public/functions.js
@@ -1,7 +1,7 @@
-// function welcome(){
-//     alert("This is a placeholder Javascript function! Expect more in the coming future.");
-//     setTimeout(3000);
-// }
+function welcome(){
+    alert("This is a placeholder Javascript function! Expect more in the coming future.");
+    setTimeout(3000);
+}
 
 // upvote toggle button functionality
 
